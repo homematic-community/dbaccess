b388e98214d22761f37140cdd43711e73f7a9458
Jens Maus <mail@jens-maus.de>
2018-09-21 15:07:38 +0200

    fix update install routines

--- generate_img.sh
+++ generate_img.sh
@@ -4,6 +4,7 @@ cp -a db/* tmp/db
 cp update_script tmp/
 cp dbaccess tmp/
 cd tmp
-tar -czvf ../dbaccess-2.0b3.tar.gz *
+find . -not -name '.DS_Store' -not -name '*.sha256' -type f -print0 | xargs -0 sha256sum >dbaccess-2.0b4.sha256
+tar --owner=root --group=root --exclude=.DS_Store -czvf ../dbaccess-2.0b4.tar.gz *
 cd ..
 rm -rf tmp
